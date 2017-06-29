78ce681fe22db8f7593477331ad112cc4836c47f
Brandon Mikelaitis <branbun2@gmail.com>
2017-06-29 21:46:14 +0000

    recaptcha message

--- mail.php
+++ mail.php
@@ -7,15 +7,20 @@ $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteve
 $responseData = json_decode($verifyResponse);
 
 if($responseData == false){
-  echo $responseData;
-    //header('Location: index.html#five');
+  echo 'Captcha was not verified, please resubmit form.  Redirecting in 5 seconds.';
+  sleep(5);
+  header('Location: index.html#five');
 }
 else {
   $curl = curl_init();
-  $name = $_POST['name']; 
+  $name = $_POST['name'];
+  $name = "Name: " + $name;
   $email = $_POST['email']; 
+  $email = "Email: " + $email;
   $subject = $_POST['subject']; 
+  $subject = "Subject: " + $subject;
   $message = $_POST['message'];
+  $message = "Message: " + $message;
   $my_env_var = getenv('SENDGRID_KEY');
   curl_setopt_array($curl, array(
     CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
@@ -35,7 +40,16 @@ else {
   $response = curl_exec($curl);
   $err = curl_error($curl);
   curl_close($curl);
+  
+  echo 'Message Sent! Your information has been sent. Redirect in 5 seconds.';
+  echo $name;
+  echo $email;
+  echo $subject;
+  echo $message;
+  sleep(5);
   header('Location: index.html');
+  
+  
   if ($err) {
     echo "cURL Error #:" . $err;
   } else {
