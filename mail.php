ce0ce781d8b96c5c8e087511a99b5d58a697aa52
EC2 Default User <ec2-user@ip-172-31-52-39.ec2.internal>
2020-06-20 22:45:02 +0000

    new email

--- mail.php
+++ mail.php
@@ -8,31 +8,42 @@ $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteve
   $responseData = json_decode($verifyResponse);
   if($responseData->success) {
   
-    $curl = curl_init();
     $name = $_POST['name'];
     $email = $_POST['email']; 
     $subject = $_POST['subject']; 
     $message = pg_escape_string($_POST['message']);
     
-    $my_env_var = getenv('SENDGRID_KEY');
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"BrandonMikelaitis@gmail.com\"\n        }\n      ],\n      \"subject\": \"New Contact\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"brandonmikelaitis@gmail.com\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"$name<br>$email<br>$subject<br>$message\"\n    }\n  ]\n}",
-      CURLOPT_HTTPHEADER => array(
-        "authorization: Bearer $my_env_var",
-        "cache-control: no-cache",
-        "content-type: application/json"
-      ),
-    ));
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
+    
+    $request_body = json_decode('{
+      "personalizations": [
+        {
+          "to": [
+            {
+              "email": "brandonmikelaitis@gmail.com"
+            }
+          ],
+          "subject": "Contact from website"
+        }
+      ],
+      "from": {
+        "email": "brandonmikelaitis@gmail.com"
+      },
+      "content": [
+        {
+          "type": "text/plain",
+          "value": "Hello, Email!"
+        }
+      ]
+    }');
+
+    $apiKey = getenv('SENDGRID_KEY');
+    $sg = new \SendGrid($apiKey);
+
+    $response = $sg->client->mail()->send()->post($request_body);
+    echo $response->statusCode();
+    echo $response->body();
+    echo $response->headers();
+
     header('Location: home.php?status=success');
     
     
